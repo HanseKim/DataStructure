926062ed9cf2ceb0f38e635dfd95bcfe4ff8a2c2
HanseKim <kimbird2002@naver.com>
2022-09-28 14:43:10 +0900

    1

---
+++ pa03_shuffle/pa03_shuffle.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+void get_list(int * list){
+    
+}
+
+void shuffle(){
+
+}
+
+void compare(){
+
+}
+
+int main(){
+    int* l1,*l2;
+    l1 = new int[5001];
+    l2 = new int[5001];
+
+    get_list(l1);
+    get_list(l2);
+
+    while(list != l1){
+        count++;
+        if(list==l2) break;
+        shuffle(l1)
+
+    }
+    while(list !=l2){
+        count++;
+        if(list==l1)break;
+        list = shuffle(l2);
+    }
+    (cnt1 < cnt2)? ():();
+}
