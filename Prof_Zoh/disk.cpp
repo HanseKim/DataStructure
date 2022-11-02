9d3b111a198632611c9577ce569aed99c3619cce
HanseKim <kimbird2002@naver.com>
2022-11-02 13:33:56 +0900

    .

---
+++ Prof_Zoh/disk.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int v;
+    cin>>v;
+    string cmd;
+    list<int> li;
+    while(1){
+        cin>>cmd;
+        if(cmd.compare("end")) break;
+        if(cmd.compare("write")){
+
+        }
+        else if(cmd.compare("show")){
+
+        }
+        else if(cmd.compare("delete")){
+
+        }
+    }
+}
